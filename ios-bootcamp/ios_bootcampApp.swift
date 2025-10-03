5d9ef1bac8a20eca3d84a220072d600a999943c7
p1an0guy <123jonahchan@gmail.com>
2025-10-03 09:06:40 -0700

    Initial Commit

---
+++ ios-bootcamp/ios_bootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_bootcampApp.swift
+//  ios-bootcamp
+//
+//  Created by Jonah Chan on 10/3/25.
+//
+
+import SwiftUI
+
+@main
+struct ios_bootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
